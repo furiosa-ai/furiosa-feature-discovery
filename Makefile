5851c1c89bc8aec321971e77037fcb3e5d2dbf28
jongchanpark-furiosa <168397600+jongchanpark-furiosa@users.noreply.github.com>
2024-09-04 03:44:38 +0900

    Add ginkgo e2e test (#13)
    * add ginkgo e2e test
    
    * use common framework for e2e-test
    
    * clean existing furiosa label before e2e test
    
    * update go module
    
    * add missing label
    
    * add Ordered to ensure sequential execution
    
    * fix convention suggestion

--- Makefile
+++ Makefile
@@ -8,15 +8,50 @@ ifndef GITHUB_TOKEN
 $(error GITHUB_TOKEN is not set. Please set the GITHUB_TOKEN environment variable)
 endif
 
+ifeq ($(shell uname -s),Darwin)
+    CGO_CFLAGS := "-I/usr/local/include"
+    CGO_LDFLAGS := "-L/usr/local/lib"
+endif
+
+LABELS_TO_REMOVE := furiosa.ai/driver.version furiosa.ai/driver.version.major furiosa.ai/driver.version.minor furiosa.ai/driver.version.patch furiosa.ai/driver.version.metadata furiosa.ai/npu.count furiosa.ai/npu.family furiosa.ai/npu.product
+
+NODES := $(shell kubectl get nodes -o name | sed 's/node\///')
+
 .PHONY: fmt
-fmt:
+fmt: fmt-rs fmt-go
+
+.PHONY: fmt-rs
+fmt-rs:
 	cargo fmt --all
 	# cargo machete # machete is not compatible with toolchain of this repo.
 	cargo sort --grouped --workspace
 
+.PHONY: fmt-go
+fmt-go:
+	go fmt ./...
+
+.PHONY: lint
+lint: clippy lint-go
+
 .PHONY: clippy
 clippy:
 	cargo fmt --all --check && cargo -q clippy --all-targets -- -D rust_2018_idioms -D warnings
+
+.PHONY: lint-go
+lint-go:
+	CGO_CFLAGS=$(CGO_CFLAGS) CGO_LDFLAGS=$(CGO_LDFLAGS) golangci-lint run
+
+.PHONY: vet
+vet:
+	CGO_CFLAGS=$(CGO_CFLAGS) CGO_LDFLAGS=$(CGO_LDFLAGS) go vet -v ./...
+
+.PHONY: tidy
+tidy:
+	go mod tidy
+
+.PHONY: vendor
+vendor:
+	go mod vendor
 
 .PHONY: clean
 clean:
@@ -45,3 +80,23 @@ image-no-cache-rel:
 .PHONY: test
 test:
 	cargo test
+
+.PHONY: clean-labels
+clean-labels:
+	@echo "Labels to remove: $(LABELS_TO_REMOVE)"
+	@for node in $(NODES); do \
+		echo "Processing node: $$node"; \
+		CURRENT_LABELS=$$(kubectl get node $$node --show-labels | grep -oP '(?<=,|^)\K[^=]+'); \
+		for label in $(LABELS_TO_REMOVE); do \
+			if echo $$CURRENT_LABELS | grep -q "$$label"; then \
+				echo "Removing label $$label from node $$node"; \
+				kubectl label node $$node $$label-; \
+			else \
+				echo "Label $$label not found on node $$node, skipping"; \
+			fi \
+		done \
+	done
+
+.PHONY:e2e-feature-discovery
+e2e-feature-discovery: clean-labels
+	CGO_CFLAGS=$(CGO_CFLAGS) CGO_LDFLAGS=$(CGO_LDFLAGS) ginkgo ./e2e
---
+++ e2e/e2e_suite_test.go
@@ -0,0 +1,90 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	framework2 "github.com/furiosa-ai/libfuriosa-kubernetes/pkg/e2e"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+)
+
+func TestE2E(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "E2E Suite")
+}
+
+func abs(path string) string {
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		panic("check path")
+	}
+
+	return absPath
+}
+
+var _ = Describe("end-to-end test", func() {
+	Context("test furiosa feature discovery", Ordered, func() {
+		It("check if Furiosa label already exists", checkNodeLabel("furiosa.ai/npu.count", false))
+
+		It("delete furiosa-feature-discovery daemonset if exists", deleteDaemonSet("furiosa-feature-discovery", "kube-system"))
+
+		It("delete node-feature-discovery daemonset if exists", deleteDaemonSet("furiosa-feature-discovery-node-feature-discovery-worker", "kube-system"))
+
+		It("deploy feature discovery helm chart", framework2.DeployHelmChart("furiosa-feature-discovery", abs("../deployments/helm"), composeValues()))
+
+		It("wait for label update", func() { time.Sleep(120 * time.Second) })
+
+		It("check if Furiosa label is updated", checkNodeLabel("furiosa.ai/npu.count", true))
+
+		It("delete helm chart", framework2.DeleteHelmChart())
+	})
+})
+
+func composeValues() string {
+	template := `namespace: kube-system
+daemonSet:
+  priorityClassName: system-node-critical
+  updateStrategy:
+    type: RollingUpdate
+    rollingUpdate:
+      maxUnavailable: 1
+  image:
+    repository: registry.corp.furiosa.ai/furiosa/furiosa-feature-discovery
+    tag: latest
+    pullPolicy: Always
+  resources:
+    cpu: 100m
+    memory: 64Mi
+`
+	return template
+}
+
+func checkNodeLabel(targetLabel string, expected bool) func() {
+	return func() {
+		nodeList, err := framework2.BackgroundContext().ClientSet.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+		Expect(err).To(BeNil())
+		Expect(len(nodeList.Items)).Should(BeNumerically(">=", 1))
+
+		for _, node := range nodeList.Items {
+			_, isExist := node.Labels[targetLabel]
+			Expect(isExist).Should(BeEquivalentTo(expected))
+		}
+	}
+}
+
+func deleteDaemonSet(targetDaemonSet string, namespace string) func() {
+	return func() {
+		err := framework2.BackgroundContext().ClientSet.AppsV1().DaemonSets(namespace).Delete(context.TODO(), targetDaemonSet, metav1.DeleteOptions{})
+
+		if err == nil {
+			fmt.Printf("DaemonSet %s already exists. Delete it\n", targetDaemonSet)
+		}
+	}
+}
